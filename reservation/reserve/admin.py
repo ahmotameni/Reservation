86ec61f7167362388690d88ecbd937b1e4045765
ahmotameni <a.h.motameni@gmail.com>
2020-04-22 13:49:11 +0430

    admin part is defined

--- reservation/reserve/admin.py
+++ reservation/reserve/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Food)
+admin.site.register(Customer)
+admin.site.register(Restaurant)
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(TableStatusPerHour)
+admin.site.register(Opinion)
+admin.site.register(VIPCustomers)
+admin.site.register(CustomersFavorites)
+

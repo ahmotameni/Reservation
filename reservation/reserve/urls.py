a11a5cf18082e636183fa34a1f9bbd69267e49f7
ahmotameni <a.h.motameni@gmail.com>
2020-04-27 00:09:49 +0430

    Back-Front connection (homepage)

--- reservation/reserve/urls.py
+++ reservation/reserve/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('menu/', views.menu, name='menu'),
+    path('', views.homepage, name='homepage'),
 ]
--- reservation/reserve/views.py
+++ reservation/reserve/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Restaurant
 
 
 def menu(request):
     foods = Food.objects.all()
-    context = {'foods': foods}
+    restaurant = Restaurant.objects.get(id=1)
+    context = {'foods': foods,
+               'restaurant': restaurant}
     return render(request, 'reserve/menu.html', context)
+
+
+def homepage(request):
+    restaurant = Restaurant.objects.get(id=1)
+    context = {'restaurant': restaurant}
+    return render(request, 'reserve/index.html', context)
